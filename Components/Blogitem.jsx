d07458f211c22c002845d50dda7ff29b3ab2e95f
eddieMus <edvaldo1.mussa@st.oth-regensburg.de>
2025-08-13 22:13:20 +0200

    Added blog data image to Blogitem component

--- Components/Blogitem.jsx
+++ Components/Blogitem.jsx
@@ -1,8 +1,13 @@
+import { blog_data } from '@/Assets/assets'
+import Image from 'next/image'
 import React from 'react'
 
 const Blogitem = () => {
   return (
-    <div>Blogitem</div>
+    <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-px_7px_0px_#000000]'>
+        <Image src={blog_data[0].image} alt='' width={400} height={400} className='border-b border-black'/>
+        <p></p>
+    </div>
   )
 }
 

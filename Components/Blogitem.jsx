e1478be0fe605664c7a5dc98b092746b9774d589
eddieMus <edvaldo1.mussa@st.oth-regensburg.de>
2025-08-13 22:09:17 +0200

    Added BlogItem component and mounted in page.js

---
+++ Components/Blogitem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Blogitem = () => {
+  return (
+    <div>Blogitem</div>
+  )
+}
+
+export default Blogitem
--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
+'use client'
+import Blogitem from "@/Components/Blogitem";
 import Header from "@/Components/Header";
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <Blogitem/>
     </>
   );
 }

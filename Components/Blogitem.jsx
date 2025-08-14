cfc065c73dab2f66e54144a327623c6e4b340f09
eddieMus <edvaldo1.mussa@st.oth-regensburg.de>
2025-08-14 19:06:16 +0200

    fixed blockitem click bug

--- Components/Blogitem.jsx
+++ Components/Blogitem.jsx
@@ -8,7 +8,6 @@ const Blogitem = ({title, description, category, image, id}) => {
     <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
         <Link href={`/blogs/${id}`}>
         <Image src={image} alt='' width={400} height={400} className='border-b border-black'/>
-        </Link>
         <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{category}</p>
         <div className='p-5'>
             <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>{title}</h5>
@@ -17,6 +16,7 @@ const Blogitem = ({title, description, category, image, id}) => {
               Read more <Image src={assets.arrow} className='ml-2' alt='' width={12}/>
             </Link>
         </div>
+        </Link>
     </div>
   )
 }

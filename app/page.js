2a981a023faac192532f412c825335332f7b5b66
eddieMus <edvaldo1.mussa@st.oth-regensburg.de>
2025-08-14 15:58:12 +0200

    Added Category selection buttons to startpage

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 'use client'
 import Blogitem from "@/Components/Blogitem";
+import BlogList from "@/Components/BlogList";
 import Header from "@/Components/Header";
 
 export default function Home() {
   return (
     <>
       <Header/>
-      <Blogitem/>
+      <BlogList/>
     </>
   );
 }

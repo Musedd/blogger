2e41577c3dbb5fe2cc7c05267b6690917dc0d5fd
eddieMus <edvaldo1.mussa@st.oth-regensburg.de>
2025-08-16 16:25:38 +0200

    setted up state variable to preview uploaded image

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -1,9 +1,22 @@
 'use client'
-import React from 'react'
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import React, { useState } from 'react'
 
 const page = () => {
+
+    const [image,setImage] = useState(false);
+
   return (
-    <div> </div>
+    <>
+        <form className='pt-5 px-5 sm:pt-12 sm:pl-16'>
+            <p className='text-xl'>Upload thumbnail</p>
+            <label htmlFor="id_image">
+                <Image className='mt-4' src={!image?assets.upload_area:URL.createObjectURL(image)} width={140} height={70} alt=''/>
+            </label>
+            <input onChange={(e) => setImage(e.target.files[0])} type="file" id='id_image' hidden required/>
+        </form>
+    </>
   )
 }
 

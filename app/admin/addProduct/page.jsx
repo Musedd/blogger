f4c9cf3045879e269304468d73138a620d075419
eddieMus <edvaldo1.mussa@st.oth-regensburg.de>
2025-08-16 17:54:21 +0200

    Blogs From admin pannel get stored in MongoDB

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -1,7 +1,9 @@
 'use client'
 import { assets } from '@/Assets/assets'
+import axios from 'axios'
 import Image from 'next/image'
 import React, { useState } from 'react'
+import { toast } from 'react-toastify'
 
 const page = () => {
 
@@ -24,16 +26,31 @@ const page = () => {
     const onSubmitHandler = async (e) => {
         e.preventDefault();
 
+        const formData = new FormData();
+        formData.append('title', data.title);
+        formData.append('description', data.description);
+        formData.append('category', data.category);
+        formData.append('author', data.author);    
+        formData.append('authorImg', data.authorImg);
+        formData.append('image',image);
+        
+        const response = await axios.post('/api/blog', formData);
+        if (response.data.success) {
+            toast.success(response.data.msg);
+        }
+        else {
+            toast.error("Error");
+        }
     }
  
   return (
     <>
         <form onSubmit={onSubmitHandler} className='pt-5 px-5 sm:pt-12 sm:pl-16'>
             <p className='text-xl'>Upload thumbnail</p>
-            <label htmlFor="id_image">
+            <label htmlFor="image">
                 <Image className='mt-4' src={!image?assets.upload_area:URL.createObjectURL(image)} width={140} height={70} alt=''/>
             </label>
-            <input onChange={(e) => setImage(e.target.files[0])} type="file" id='id_image' hidden required/>
+            <input onChange={(e) => setImage(e.target.files[0])} type="file" id='image' hidden required/>
             <p className='text-xl mt-4'>Blog title</p>
             <input name='title' onChange={onChangeHandler} value={data.title} className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='Type here' required />
             <p className='text-xl mt-4'>Blog description</p>

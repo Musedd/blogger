8fe1e2c88453c76656e0ce1f18c94a27da31f286
eddieMus <edvaldo1.mussa@st.oth-regensburg.de>
2025-08-16 16:38:30 +0200

    finished frontend for uploading blog

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -6,6 +6,13 @@ import React, { useState } from 'react'
 const page = () => {
 
     const [image,setImage] = useState(false);
+    const [data,setData] = useState({
+        title: "",
+        description:"",
+        category:"Startup",
+        author:"Alex B",
+        authorImg:"/author_img.png"
+    })
 
   return (
     <>
@@ -15,6 +22,18 @@ const page = () => {
                 <Image className='mt-4' src={!image?assets.upload_area:URL.createObjectURL(image)} width={140} height={70} alt=''/>
             </label>
             <input onChange={(e) => setImage(e.target.files[0])} type="file" id='id_image' hidden required/>
+            <p className='text-xl mt-4'>Blog title</p>
+            <input className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='Type here' required />
+            <p className='text-xl mt-4'>Blog description</p>
+            <textarea className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='write content here' rows={6} required />
+            <p className='text-xl mt-4'>Blog category</p>
+            <select name="Category" className='w-40 mt-4 px-4 py-3 border text-gray-500'>
+                <option value="Startup">Startup</option>
+                <option value="Technology">Technology</option>
+                <option value="Lifestyle">Lifestyle</option>
+            </select>
+            <br />
+            <button type="submit" className='mt-8 w-40 h-12 bg-black text-white'>ADD</button>
         </form>
     </>
   )

f689206d7ee3f78db4eb4be276c998d647a7fd4b
eddieMus <edvaldo1.mussa@st.oth-regensburg.de>
2025-08-16 16:47:18 +0200

    Data gets saved in Object and refresh gets blocked

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -14,20 +14,32 @@ const page = () => {
         authorImg:"/author_img.png"
     })
 
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data=>({...data,[name]:value}));
+        console.log(data);
+    }
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+
+    }
+ 
   return (
     <>
-        <form className='pt-5 px-5 sm:pt-12 sm:pl-16'>
+        <form onSubmit={onSubmitHandler} className='pt-5 px-5 sm:pt-12 sm:pl-16'>
             <p className='text-xl'>Upload thumbnail</p>
             <label htmlFor="id_image">
                 <Image className='mt-4' src={!image?assets.upload_area:URL.createObjectURL(image)} width={140} height={70} alt=''/>
             </label>
             <input onChange={(e) => setImage(e.target.files[0])} type="file" id='id_image' hidden required/>
             <p className='text-xl mt-4'>Blog title</p>
-            <input className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='Type here' required />
+            <input name='title' onChange={onChangeHandler} value={data.title} className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='Type here' required />
             <p className='text-xl mt-4'>Blog description</p>
-            <textarea className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='write content here' rows={6} required />
+            <textarea name='description' onChange={onChangeHandler} value={data.description} className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='write content here' rows={6} required />
             <p className='text-xl mt-4'>Blog category</p>
-            <select name="Category" className='w-40 mt-4 px-4 py-3 border text-gray-500'>
+            <select name="Category" onChange={onChangeHandler} value={data.category} className='w-40 mt-4 px-4 py-3 border text-gray-500'>
                 <option value="Startup">Startup</option>
                 <option value="Technology">Technology</option>
                 <option value="Lifestyle">Lifestyle</option>
